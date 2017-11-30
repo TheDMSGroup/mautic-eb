c425c2e93d996f6a5c39ff681c17ed7124377df8
heathdutton <hdutton@thedmsgrp.com>
2017-11-30 08:30:34 -0500

    Upgraded to 2.11.0 and switched to using composer w/ vcs.

---
+++ mautic_custom/app/config/paths_local.php
@@ -0,0 +1,14 @@
+<?php
+
+$paths = array_merge($paths, [
+    'themes'       => 'themes',
+    'assets'       => 'media',
+    'asset_prefix' => '',
+    'plugins'      => 'plugins',
+    'translations' => 'translations',
+    'local_config' => '%kernel.root_dir%/config/local.php',
+    'root'         => substr($root, 0, -4),
+    'app'          => 'app',
+    'bundles'      => 'app/bundles',
+    'vendor'       => 'vendor',
+]);
