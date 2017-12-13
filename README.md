0cb958e410acc6857091c6ffb8f4461645071f4e
heathdutton <hdutton@thedmsgrp.com>
2017-12-13 17:08:07 -0500

    Separate NR APM from INF installation.
    Someone may be only subscribed to one or the other.

--- README.md
+++ README.md
@@ -23,7 +23,8 @@ Deploy Mautic in a fully-scalable Amazon Elastic Beanstalk cluster.
     APP_URL               - Domain upon which you are running by default.
     EFS_DNS_NAME          - The full DNS of the EFS mount.
     MAUTIC_INSTALL        - Set to "1" to initialize mautic for the first time.
-    NR_INSTALL_KEY        - Optional NewRelic install key.
+    NR_APM_INSTALL_KEY    - Optional NewRelic install key for Application Monitoring.
+    NR_INF_INSTALL_KEY    - Optional NewRelic install key for Infrastructure.
 
 ### Bitbucket Pipelines / Travis CI Environment Variables
 This repo can be used to automatically build and deploy Mautic to Elastic Beanstalk using Bitbucket pipelines. To do so, BitBucket Pipelines must be enabled for this repo, and the following environment variables must be added to Pipeline settings:
--- scripts/newrelic-start.sh
+++ scripts/newrelic-start.sh
@@ -5,12 +5,6 @@
 
 . /opt/elasticbeanstalk/support/envvars
 
-if [ -z "$NR_INSTALL_KEY" ]
-then
-    echo "Please set the global variable NR_INSTALL_KEY if you wish to use NewRelic reporting."
-    exit 0
-fi
-
 if [ -z "$NR_APPNAME" ]
 then
     echo "Please set the global variable NR_APPNAME if you wish to use NewRelic reporting."
@@ -30,23 +24,34 @@ newrelic.error_collector.record_database_errors = false
 newrelic.framework = "symfony2"
 EOM
 
-chmod 000644 /etc/php.d/newrelic.ini
-chown root:root /etc/php.d/newrelic.ini
+if [ !-z "$NR_APM_INSTALL_KEY" ]
+then
 
-export NR_INSTALL_KEY=$NR_INSTALL_KEY
-export NR_INSTALL_SILENT=true
+    chmod 000644 /etc/php.d/newrelic.ini
+    chown root:root /etc/php.d/newrelic.ini
 
-# Start the Application monitor.
-newrelic-install install
+    export NR_INSTALL_KEY=$NR_INSTALL_KEY
+    export NR_INSTALL_SILENT=true
 
-# Start the System monitor.
-nrsysmond-config --set license_key=$NR_INSTALL_KEY
-sudo /etc/init.d/newrelic-sysmond restart
+    # Start the Application monitor.
+    newrelic-install install
+else
+    echo "Please set the global variable NR_APM_INSTALL_KEY if you wish to use NewRelic APM reporting."
+fi
 
-# Start the Infrastructure monitor.
-echo "license_key: $NR_INSTALL_KEY" | sudo tee -a /etc/newrelic-infra.yml
-printf "[newrelic-infra]\nname=New Relic Infrastructure\nbaseurl=http://download.newrelic.com/infrastructure_agent/linux/yum/el/6/x86_64\nenable=1\ngpgcheck=0" | sudo tee -a /etc/yum.repos.d/newrelic-infra.repo
+if [ !-z "$NR_INF_INSTALL_KEY" ]
+then
+    # Start the System monitor.
+    nrsysmond-config --set license_key=$NR_INSTALL_KEY
+    sudo /etc/init.d/newrelic-sysmond restart
 
-yum-config-manager --save --setopt=newrelic-infra.skip_if_unavailable=true
-sudo yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
-sudo yum install newrelic-infra -y
+    # Start the Infrastructure monitor.
+    echo "license_key: $NR_INSTALL_KEY" | sudo tee -a /etc/newrelic-infra.yml
+    printf "[newrelic-infra]\nname=New Relic Infrastructure\nbaseurl=http://download.newrelic.com/infrastructure_agent/linux/yum/el/6/x86_64\nenable=1\ngpgcheck=0" | sudo tee -a /etc/yum.repos.d/newrelic-infra.repo
+
+    yum-config-manager --save --setopt=newrelic-infra.skip_if_unavailable=true
+    sudo yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
+    sudo yum install newrelic-infra -y
+else
+    echo "Please set the global variable NR_INF_INSTALL_KEY if you wish to use NewRelic Infrastructure reporting."
+fi
