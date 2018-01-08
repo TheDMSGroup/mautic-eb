ea8fd672b3b4f6b741650666d381d81d55e3330f
heathdutton <hdutton@thedmsgrp.com>
2018-01-08 11:21:42 -0500

    Add config file for prod.
    Activates APCu.

---
+++ mautic_eb/app/config/config_prod.php
@@ -0,0 +1,75 @@
+<?php
+
+$loader->import('config.php');
+
+if (file_exists(__DIR__.'/security_local.php')) {
+    $loader->import('security_local.php');
+} else {
+    $loader->import('security.php');
+}
+
+$container->loadFromExtension("framework", array(
+    "validation" => array(
+        "cache" => "apc"
+    )
+));
+
+$container->loadFromExtension("doctrine", array(
+    "orm" => array(
+        "metadata_cache_driver" => "apc",
+        "result_cache_driver"   => "apc",
+        "query_cache_driver"    => "apc"
+    )
+));
+
+$debugMode = $container->hasParameter('mautic.debug') ? $container->getParameter('mautic.debug') : $container->getParameter('kernel.debug');
+
+$container->loadFromExtension('monolog', [
+    'channels' => [
+        'mautic',
+    ],
+    'handlers' => [
+        'main' => [
+            'formatter'    => $debugMode ? 'mautic.monolog.fulltrace.formatter' : null,
+            'type'         => 'fingers_crossed',
+            'buffer_size'  => '200',
+            'action_level' => ($debugMode) ? 'debug' : 'error',
+            'handler'      => 'nested',
+            'channels'     => [
+                '!mautic',
+            ],
+        ],
+        'nested' => [
+            'type'      => 'rotating_file',
+            'path'      => '%kernel.logs_dir%/%kernel.environment%.php',
+            'level'     => ($debugMode) ? 'debug' : 'error',
+            'max_files' => 7,
+        ],
+        'mautic' => [
+            'formatter' => $debugMode ? 'mautic.monolog.fulltrace.formatter' : null,
+            'type'      => 'rotating_file',
+            'path'      => '%kernel.logs_dir%/mautic_%kernel.environment%.php',
+            'level'     => ($debugMode) ? 'debug' : 'notice',
+            'channels'  => [
+                'mautic',
+            ],
+            'max_files' => 7,
+        ],
+    ],
+]);
+
+//Twig Configuration
+$container->loadFromExtension('twig', [
+    'cache'       => '%mautic.tmp_path%/%kernel.environment%/twig',
+    'auto_reload' => true,
+]);
+
+// Allow overriding config without a requiring a full bundle or hacks
+if (file_exists(__DIR__.'/config_override.php')) {
+    $loader->import('config_override.php');
+}
+
+// Allow local settings without committing to git such as swift mailer delivery address overrides
+if (file_exists(__DIR__.'/config_local.php')) {
+    $loader->import('config_local.php');
+}
