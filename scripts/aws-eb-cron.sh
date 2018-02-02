9d14d7f214def616c06851548dbb8f9744bd55ee
heathdutton <hdutton@thedmsgrp.com>
2018-02-02 15:56:00 -0500

    Fix bash flaws with cron script.
    Closes #2

--- scripts/aws-eb-cron.sh
+++ scripts/aws-eb-cron.sh
@@ -16,13 +16,13 @@
 #   * * * * * php cron.php
 
 # Load environment variables if available.
-if [ -f "/opt/elasticbeanstalk/support/envvars" ]
+if [[ -f "/opt/elasticbeanstalk/support/envvars" ]]
 then
     . /opt/elasticbeanstalk/support/envvars
 fi
 
 # Store the previous value established (if available)
-if [ -z "$AWS_EB_CRON" ]
+if [[ -z "$AWS_EB_CRON" ]]
 then
     PREV_AWS_EB_CRON="$AWS_EB_CRON"
 else
@@ -30,11 +30,11 @@ else
 fi
 
 # Establish the frequency of lead checking.
-if [ -z "$AWS_EB_CRON_FREQ" ]
+if [[ -z "$AWS_EB_CRON_FREQ" ]]
 then
     # By default, check every 5 minutes.
     # This is because it requires 4 curl actions, so you don't wanna run it ALL the time.
-    AWS_EB_CRON_FREQ=300
+    AWS_EB_CRON_FREQ=5
 fi
 
 # Check if we've already established the leader recently.
@@ -47,7 +47,7 @@ else
 
     # Get the current instance ID.
     EC2_INSTANCE_ID=$( curl -s -m 5 http://169.254.169.254/latest/meta-data/instance-id )
-    if [ -z "$EC2_INSTANCE_ID" ]
+    if [[ -z "$EC2_INSTANCE_ID" ]]
     then
         echo "Could not determine my EC2 instance. You may not be running this in an Amazon instance or something has gone terribly wrong. Aborting."
         exit
@@ -63,7 +63,7 @@ else
 
     # Get the elastic beanstalk environment id.
     EB_ENV_ID=$( aws ec2 describe-instances --region "$EC2_REGION" --instance-id "$EC2_INSTANCE_ID" --output text | grep "TAGS.*elasticbeanstalk:environment-id.*" | sed -e 's/TAGS\selasticbeanstalk:environment-id\s//' )
-    if [ -z "$EB_ENV_ID" ]
+    if [[ -z "$EB_ENV_ID" ]]
     then
         echo "Could not determine my EB Environment ID. Perhaps you are not running this in Elastic Beanstalk? Aborting."
         exit
@@ -73,14 +73,14 @@ else
     # Get all running instances with this tag, just the instance ID and the LaunchTime.
     EC2_INSTANCES=$( aws ec2 describe-instances --region "$EC2_REGION" --output text --query 'Reservations[*].Instances[*].[LaunchTime,InstanceId]' --filters "Name=tag-key,Values=elasticbeanstalk:environment-id" "Name=tag-value,Values=$EB_ENV_ID" "Name=instance-state-code,Values=16" )
 
-    if [ -z "$EC2_INSTANCES" ]
+    if [[ -z "$EC2_INSTANCES" ]]
     then
         echo "No running sibling instances found, so I am the leader."
         AWS_EB_CRON=1
     else
         # The last instance in a sorted list will be the oldest, and will be our leader.
         EC2_INSTANCE_LEAD_ID=$( echo $EC2_INSTANCES | sort | tail -n 1 | sed -e 's/^.*\s//' )
-        if [ "$EC2_INSTANCE_LEAD_ID" -eq "$EC2_INSTANCE_ID" ]
+        if [[ "$EC2_INSTANCE_LEAD_ID" == "$EC2_INSTANCE_ID" ]]
         then
             echo "I am the oldest instance running, so I am the leader."
             AWS_EB_CRON=1
@@ -91,11 +91,11 @@ else
     fi
 
     # Update the current instance environment variable "AWS_EB_CRON" if needed.
-    if [ "$AWS_EB_CRON" -ne "$PREV_AWS_EB_CRON" ]
+    if [[ "$AWS_EB_CRON" != "$PREV_AWS_EB_CRON" ]]
     then
         # This is a change from previously known status.
         sudo chmod 0646 /opt/elasticbeanstalk/support/envvars
-        if [ -z "$( grep 'export AWS_EB_CRON=' /opt/elasticbeanstalk/support/envvars )" ]
+        if [[ -z "$( grep 'export AWS_EB_CRON=' /opt/elasticbeanstalk/support/envvars )" ]]
         then
             echo "Adding a new variable."
             sudo echo "export AWS_EB_CRON=\"$AWS_EB_CRON\"" >> /opt/elasticbeanstalk/support/envvars
@@ -107,11 +107,11 @@ else
     fi
 
     # Store the value in a file in /tmp for quicker lookup during the check_frequency.
-    echo $AWS_EB_CRON > /tmp/AWS_EB_CRON
+    echo "$AWS_EB_CRON" > /tmp/AWS_EB_CRON
 fi
 
 # Run commands following this script as parameters if we are the leader.
-if [[ "$AWS_EB_CRON" -eq "1" ]]
+if [[ "$AWS_EB_CRON" == "1" ]]
 then
     if [[ ! -z "$@" ]]
     then
