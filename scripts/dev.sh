a10551aa198816f5b7ff09071538e8669c49061a
heathdutton <hdutton@thedmsgrp.com>
2018-03-26 17:46:51 -0400

    Update dev.sh to pull from default branch.
    We are now using git-flow-ish for these plugins.

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -6,16 +6,18 @@ cd $BASEDIR/../
 BASEDIR=$( pwd )
 
 rm -rf ./plugins/MauticContactClientBundle
-git clone https://github.com/TheDMSGroup/mautic-contact-client.git ./plugins/MauticContactClientBundle
+git clone -b master https://github.com/TheDMSGroup/mautic-contact-client.git ./plugins/MauticContactClientBundle
 
 rm -rf ./plugins/MauticContactSourceBundle
-git clone https://github.com/TheDMSGroup/mautic-contact-source.git ./plugins/MauticContactSourceBundle
+git clone -b master https://github.com/TheDMSGroup/mautic-contact-source.git ./plugins/MauticContactSourceBundle
 
 rm -rf ./plugins/MauticEnhancerBundle
-git clone https://github.com/TheDMSGroup/mautic-enhancer.git ./plugins/MauticEnhancerBundle
+git clone -b master https://github.com/TheDMSGroup/mautic-enhancer.git ./plugins/MauticEnhancerBundle
 
 rm -rf ./plugins/MauticExtendedFieldBundle
-git clone https://github.com/TheDMSGroup/mautic-extended-field.git ./plugins/MauticExtendedFieldBundle
+git clone -b master https://github.com/TheDMSGroup/mautic-extended-field.git ./plugins/MauticExtendedFieldBundle
 
 rm -rf ./plugins/MauticContactLedgerBundle
-git clone https://github.com/TheDMSGroup/mautic-contact-ledger.git ./plugins/MauticContactLedgerBundle
+git clone -b master https://github.com/TheDMSGroup/mautic-contact-ledger.git ./plugins/MauticContactLedgerBundle
+
+composer custom
