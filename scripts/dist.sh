6fb9af9c81606c24239ed4b0d7b1f8f6b016c2cd
heathdutton <hdutton@thedmsgrp.com>
2018-04-02 17:11:38 -0400

    Dependancy updates and new dist script to bypass upstream changes.

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 # Rebuild all assets in place to test composer changes and prep for release.
+# Will not update the core composer.lock file (to do so delete it first).
 
 BASEDIR=$(dirname "$BASH_SOURCE")
 cd $BASEDIR/../
 BASEDIR=$( pwd )
 
-rm -rf ./mautic ./bin ./vendor ./composer.lock ./plugins ./mautic_custom
+rm -rf ./mautic ./bin ./vendor ./plugins ./mautic_custom
 mkdir -p ./plugins
 touch ./plugins/.gitkeep
-mkdir -p ./mautic_custom
-touch ./mautic_custom/.gitkeep
 
+cp composer.lock.dist composer.lock
 cp composer.custom.dist composer.custom
 
 rm -rf ~/.composer/cache/files/thedmsgroup/*
-# composer clear-cache
 composer install
 git status
+composer update "thedmsgroup/*"
 composer assets
 
 if cat ./mautic/media/css/app.css | grep '#00b49c' > /dev/null 2>&1
@@ -27,6 +27,8 @@ then
 	if cat ./mautic/media/css/app.css | grep '#00b49c' > /dev/null 2>&1
 	then
 	    echo "No luck. Abort build!"
+	else
+	    echo "And they look good."
 	fi
     exit 1
 fi
