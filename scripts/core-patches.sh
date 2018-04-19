ddf9e18379659524ea7ae3c34a4793640a237c20
heathdutton <hdutton@thedmsgrp.com>
2018-04-19 09:46:56 -0400

    Refactor dist script.

---
+++ scripts/core-patches.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Applying core Mautic patches that have not yet been merged in to the core version we are currently using.
+# Using bash instead of composer so that we do not alter other dependencies outside of the core scope.
+
+BASEDIR=$(dirname "$BASH_SOURCE")
+cd $BASEDIR/../
+BASEDIR=$( pwd )
+
+echo ; echo "Applying important patches to Mautic core."
+cd ./mautic
+
+echo ; echo "[Bug] Plugin content locking (referrer POST) causes 500. #5789"
+curl -L "https://github.com/mautic/mautic/pull/5789.diff" | git apply -v
+
+echo ; echo "[Enhancement] Allow filtering contacts by UTM data within campaigns. #5869"
+curl -L "https://github.com/mautic/mautic/pull/5869.diff" | git apply -v
+
+echo ; echo "[Bug] SQL error if no field is chosen for \"Form field value\" conditional. #5875"
+curl -L "https://github.com/mautic/mautic/pull/5875.diff" | git apply -v
+
+echo ; echo "[Enhancement] Allow filtering contacts by UTM data for segments. #5886"
+curl -L "https://github.com/mautic/mautic/pull/5886.diff" | git apply -v
+
+# echo ; echo "[Enhancement] Allow filtering contacts by Campaign Membership for segments. #5911"
+# curl -L "https://github.com/mautic/mautic/pull/5911.diff" | git apply -v
+
+echo ; echo "[Enhancement] Support includes/excludes with text fields for bulk filtering. #5925"
+curl -L "https://gist.githubusercontent.com/heathdutton/6542c12e1f55c0c3d3cbe6fe51706728/raw/164cec9fde0bd56845b40285ae4579b28cd476a7/5925.diff" | git apply -v
+
+echo ; echo "[Bug] Symfony Master/Slave support is broken #5969"
+curl -L "https://github.com/mautic/mautic/pull/5970.diff" | git apply -v
+
--- scripts/dist.sh
+++ scripts/dist.sh
@@ -6,48 +6,28 @@ BASEDIR=$(dirname "$BASH_SOURCE")
 cd $BASEDIR/../
 BASEDIR=$( pwd )
 
+echo "Cleaning up the build space."
 rm -rf ./mautic ./bin ./vendor ./plugins ./mautic_custom
 mkdir -p ./plugins
 touch ./plugins/.gitkeep
 
+echo ; echo "Setting composer.lock and composer.custom files from the dist coppies."
 cp composer.lock.dist composer.lock
 cp composer.custom.dist composer.custom
 
+echo ; echo "Updating mautic-eb custom plugin dependencies."
 rm -rf ~/.composer/cache/files/thedmsgroup/*
 composer install --no-interaction
 git status
 cd ./mautic_custom ; git pull ; cd -
 composer update "thedmsgroup/*" --no-interaction
 
-echo "Applying patches"
-cd ./mautic
-echo "[Bug] Plugin content locking (referrer POST) causes 500. #5789"
-curl -L "https://github.com/mautic/mautic/pull/5789.diff" | git apply -v
+bash ./scripts/core-patches.sh
 
-echo "[Enhancement] Allow filtering contacts by UTM data within campaigns. #5869"
-curl -L "https://github.com/mautic/mautic/pull/5869.diff" | git apply -v
-
-echo "[Bug] SQL error if no field is chosen for \"Form field value\" conditional. #5875"
-curl -L "https://github.com/mautic/mautic/pull/5875.diff" | git apply -v
-
-echo "[Enhancement] Allow filtering contacts by UTM data for segments. #5886"
-curl -L "https://github.com/mautic/mautic/pull/5886.diff" | git apply -v
-
-#echo "[Enhancement] Allow filtering contacts by Campaign Membership for segments. #5911"
-#curl -L "https://github.com/mautic/mautic/pull/5911.diff" | git apply -v
-
-echo "[Enhancement] Support includes/excludes with text fields for bulk filtering. #5925"
-curl -L "https://gist.githubusercontent.com/heathdutton/6542c12e1f55c0c3d3cbe6fe51706728/raw/164cec9fde0bd56845b40285ae4579b28cd476a7/5925.diff" | git apply -v
-
-echo "[Bug] Symfony Master/Slave support is broken #5969"
-curl -L "https://github.com/mautic/mautic/pull/5970.diff" | git apply -v
-
-echo "Rejected patches:"
-find . -name \*.rej
-cd -
-
+echo ; echo "Compiling Mautic JS/CSS assets."
 composer assets --no-interaction
-echo "Checking assets..."
+
+echo ; echo "Checking assets."
 if cat ./mautic/media/css/app.css | grep '#00b49c' > /dev/null 2>&1
 then
     echo "Warning. Default bootstrap styles detected. Theme compilation must have failed."
@@ -57,7 +37,7 @@ then
 	then
 	    echo "No luck. Abort build!"
 	else
-	    echo "And they look good."
+	    echo "And they look good (on second try)."
 	fi
     exit 1
 else
