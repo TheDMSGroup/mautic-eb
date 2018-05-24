a990ef2cd34cdaaaef000c7aca87a25d0bd0183d
cykonetic <nicholaibush@yahoo.com>
2018-05-24 09:43:00 -0400

    merge conflict fixes and php-cs-fixer run

--- mautic_eb/app/config/local.php
+++ mautic_eb/app/config/local.php
@@ -1,162 +1,251 @@
 <?php
-/**
- * Global default environment variable overrides for Mautic EB
- * To override for your instance use parameters_local.php
- */
-$parameters = [
-    'db_driver'         => 'pdo_mysql',
-    'db_table_prefix'   => null,
-    'db_host'           => getenv('DB_HOST') ?: $parameters['db_host'] ?? '127.0.0.1',
-    'db_host_ro'        => getenv('DB_HOST_RO') ?: null,
-    'db_port'           => getenv('DB_PORT') ?: $parameters['db_port'] ?? '3306',
-    'db_name'           => getenv('DB_NAME') ?: $parameters['db_name'] ?? 'mautic_eb_site_0',
-    'db_user'           => getenv('DB_USER') ?: $parameters['db_user'] ?? 'root',
-    'db_password'       => getenv('DB_PASSWD') ?: $parameters['db_password'] ?? '',
-    'mailer_from_name'  => getenv('MAILER_FROM_NAME') ?: $parameters['mailer_from_name'] ?? 'Web Developer',
-    'mailer_from_email' => getenv('MAILER_FROM_EMAIL') ?: $parameters['mailer_from_email'] ?? 'web@developer.com',
-    'mailer_transport'  => 'mail',
-    'mailer_host'       => null,
-    'mailer_port'       => null,
-    'mailer_user'       => 'root',
-    'mailer_password'   => 'root',
-    'mailer_encryption' => null,
-    'mailer_auth_mode'  => null,
-    'mailer_spool_type' => 'file',
-    'mailer_spool_path' => '%kernel.root_dir%/spool',
-    'secret_key'        => getenv('SECRET_KEY') ?: '3e29c87bddfbfc8e59d004581da4fa9f5c9fe0a9f1f90a244a38e2e5600c2800',
-    'site_url'          => getenv('APP_URL') ?: 'http://mautic.loc',
-    // Always use the core system tmp path.
-    'tmp_path'          => '/tmp',
-];
+$parameters = array(
+	'db_driver' => 'pdo_mysql',
+	'db_host' => 'localhost',
+	'db_table_prefix' => null,
+	'db_port' => '3306',
+	'db_name' => 'mautic_eb',
+	'db_user' => 'mautic',
+	'db_password' => 'mautic',
+	'db_backup_tables' => 1,
+	'db_backup_prefix' => 'bak_',
+	'db_server_version' => '5.7.22-0ubuntu0.17.10.1',
+	'mailer_from_name' => 'Mautic Admin',
+	'mailer_from_email' => 'nbush@thedmsgrp.com',
+	'mailer_transport' => 'sendmail',
+	'mailer_host' => null,
+	'mailer_port' => null,
+	'mailer_user' => null,
+	'mailer_password' => null,
+	'mailer_encryption' => null,
+	'mailer_auth_mode' => null,
+	'mailer_spool_type' => 'memory',
+	'mailer_spool_path' => '%kernel.root_dir%/spool',
+	'secret_key' => '9b386ede833912836ab00d09154413c35b312b4d90769fcc263bd89cf217222a',
+	'site_url' => 'http://mautic.local',
+	'webroot' => null,
+	'cache_path' => '/home/nbush/workspace/mautic-eb/mautic/app/cache',
+	'log_path' => '/home/nbush/workspace/mautic-eb/mautic/app/logs',
+	'image_path' => 'media/images',
+	'tmp_path' => '/home/nbush/workspace/mautic-eb/mautic/app/cache',
+	'theme' => 'Mauve',
+	'locale' => 'en_US',
+	'dev_hosts' => null,
+	'trusted_hosts' => array(
 
-/**
- * Multisite environment variable overrides based on the inbound domain.
- *
- * APCu required for performance.
- * Low level logic is intentional since we are in a bootstrap requirement.
- */
-if (!function_exists('mauticEBMultisite')) {
-    function mauticEBMultisite(&$parameters)
-    {
-        if (
-            boolval(getenv('EB_MULT'))
-            && isset($_SERVER['HTTP_HOST'])
-        ) {
-            $site = null;
-            $host = strtolower(filter_var($_SERVER['HTTP_HOST'], FILTER_SANITIZE_URL));
-            $key  = 'mautic_eb_mult_host_'.$host;
-            if (isset($GLOBALS['mautic_eb_mult_site'])) {
-                $site = $GLOBALS['mautic_eb_mult_site'];
-            }
-            if (!$site && function_exists('apcu_fetch')) {
-                $site = apcu_fetch($key);
-            }
-            if (!$site) {
-                // Get parameters for the multi-site MySQL database.
-                // Optionally allow a completely different connection for this data.
-                $ebParams = [
-                    'db_host'     => getenv('EB_MULTI_DB_HOST') ?: $parameters['db_host'],
-                    'db_port'     => getenv('EB_MULTI_DB_PORT') ?: $parameters['db_port'],
-                    'db_name'     => getenv('EB_MULTI_DB_NAME') ?: 'mautic_eb_multi',
-                    'db_user'     => getenv('EB_MULTI_DB_USER') ?: $parameters['db_user'],
-                    'db_password' => getenv('EB_MULTI_DB_PASSWD') ?: $parameters['db_password'],
-                ];
-                if (!is_string($ebParams['db_host']) || !is_string($ebParams['db_user']) || !is_string(
-                        $ebParams['db_password']
-                    ) || !is_string($ebParams['db_name']) || !is_string($ebParams['db_port'])) {
-                    throw \Exception('Invalid site host parameters.');
-                }
-                $mysqli = new mysqli(
-                    $ebParams['db_host'], $ebParams['db_user'], $ebParams['db_password'],
-                    $ebParams['db_name'], $ebParams['db_port']
-                );
-                if (mysqli_connect_error()) {
-                    throw \Exception('Unable to connect to EB_MULTI host.', mysqli_connect_error());
-                } else {
-                    $query  = "SELECT `sites`.* FROM `hosts` LEFT JOIN `sites` ON `hosts`.`site_id` = `sites`.`id` WHERE `hosts`.`host` = '".$mysqli->real_escape_string(
-                            $host
-                        )."' LIMIT 1;";
-                    $result = $mysqli->query($query);
-                    if (!$result) {
-                        throw \Exception('This domain is not currently supported.');
-                    } else {
-                        $array = $result->fetch_array();
-                        // 0 = waiting for install, 1 = creating storage, 2 = creating database, 3 = running, 4 = halted, 5 = waiting to resume, 6 = waiting for decommission, 7 = decommission
-                        switch ($array['status']) {
-                            case 0:
-                                throw \Exception('This site is awaiting setup.');
+	),
+	'trusted_proxies' => array(
 
-                            case 1:
-                                throw \Exception('Storage is being allocated for this site.');
+	),
+	'rememberme_key' => 'ae1d0e90c645495851c4895c7ce184983bc8764e',
+	'rememberme_lifetime' => '31536000',
+	'rememberme_path' => '/',
+	'default_pagelimit' => 30,
+	'default_timezone' => 'UTC',
+	'date_format_full' => 'F j, Y g:i a T',
+	'date_format_short' => 'D, M d',
+	'date_format_dateonly' => 'F j, Y',
+	'date_format_timeonly' => 'g:i a',
+	'ip_lookup_service' => 'maxmind_download',
+	'ip_lookup_auth' => null,
+	'ip_lookup_config' => array(
 
-                            case 2:
-                                throw \Exception('A database is being created for this site.');
+	),
+	'update_stability' => 'stable',
+	'cookie_path' => '/',
+	'cookie_secure' => null,
+	'do_not_track_ips' => array(
 
-                            case 3:
-                                // Site is running and enabled.
-                                $site = $array;
-                                break;
+	),
+	'do_not_track_bots' => array(
+		"0" => "MSNBOT", 
+		"1" => "msnbot-media", 
+		"2" => "bingbot", 
+		"3" => "Googlebot", 
+		"4" => "Google Web Preview", 
+		"5" => "Mediapartners-Google", 
+		"6" => "Baiduspider", 
+		"7" => "Ezooms", 
+		"8" => "YahooSeeker", 
+		"9" => "Slurp", 
+		"10" => "AltaVista", 
+		"11" => "AVSearch", 
+		"12" => "Mercator", 
+		"13" => "Scooter", 
+		"14" => "InfoSeek", 
+		"15" => "Ultraseek", 
+		"16" => "Lycos", 
+		"17" => "Wget", 
+		"18" => "YandexBot", 
+		"19" => "Java/1.4.1_04", 
+		"20" => "SiteBot", 
+		"21" => "Exabot", 
+		"22" => "AhrefsBot", 
+		"23" => "MJ12bot", 
+		"24" => "NetSeer crawler", 
+		"25" => "TurnitinBot", 
+		"26" => "magpie-crawler", 
+		"27" => "Nutch Crawler", 
+		"28" => "CMS Crawler", 
+		"29" => "rogerbot", 
+		"30" => "Domnutch", 
+		"31" => "ssearch_bot", 
+		"32" => "XoviBot", 
+		"33" => "digincore", 
+		"34" => "fr-crawler", 
+		"35" => "SeznamBot", 
+		"36" => "Seznam screenshot-generator", 
+		"37" => "Facebot", 
+		"38" => "facebookexternalhit"
+	),
+	'do_not_track_internal_ips' => array(
 
-                            case 4:
-                                throw \Exception('This site has been temporarily disabled.');
+	),
+	'link_shortener_url' => null,
+	'cached_data_timeout' => '10',
+	'batch_sleep_time' => '1',
+	'cors_restrict_domains' => 0,
+	'cors_valid_domains' => array(
 
-                            case 5:
-                                throw \Exception('This site is being restored to active duty.');
-
-                            case 6:
-                                throw \Exception('This site is being decommissioned permanently.');
-
-                            case 7:
-                                throw \Exception('This site has been decommissioned permanently.');
-                        }
-                    }
-                }
-                // Cache this site locally in APCu for quicker retrieval.
-                if ($site && function_exists('apcu_store')) {
-                    apcu_store($key, $site, 300);
-                }
-                $GLOBALS['mautic_eb_mult_site'] = $site;
-            }
-            // If we have discerned the site, and it is active, it changes our parameters from the default.
-            if ($site) {
-                if ($_COOKIE['EB_MULTI_SITE_ID'] !== $site['id']) {
-                    // Global cookie for route filtering at the apache level for later security.
-                    setcookie('EB_MULTI_SITE_ID', $site['id'], time() + 604800, '/', $site['host'], false, false);
-                }
-
-                // Override parameters array with the JSON array in the parameters column if possible.
-                if (!empty($site['parameters'])) {
-                    $jsonParams = json_decode($site['parameters']);
-                    if (!$jsonParams || !is_array($jsonParams)) {
-                        throw \Exception('The custom parameters for this site are malformed.');
-                    } else {
-                        $parameters = array_merge($parameters, $jsonParams);
-                    }
-                }
-
-                // Set the path for log entry output.
-                $parameters['log_path'] = 'multi/'.$site['id'].'/logs';
-
-                // Set the path for images.
-                $parameters['image_path'] = 'multi/'.$site['id'].'/media/images';
-
-                // Set the path for generic uploads.
-                $parameters['upload_dir'] = 'multi/'.$site['id'].'/media/files';
-
-                // Set the database name.
-                $parameters['db_name'] = 'mautic_eb_site_'.$site['id'];
-
-                // Explicitly set the site URL based on the inbound host, allow flexible SSL.
-                if (
-                    isset($_SERVER['HTTP_X_FORWARDED_PROTO'])
-                    && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'
-                ) {
-                    $_SERVER['HTTPS'] = 'on';
-                }
-                $protocol = (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) != 'off') ? 'https' : 'http';
-                $parameters['site_url'] = $protocol . '://' . $host;
-            }
-        }
-    }
-}
-mauticEBMultisite($parameters);
+	),
+	'rss_notification_url' => 'https://mautic.com/?feed=rss2&tag=notification',
+	'max_entity_lock_time' => 0,
+	'api_enabled' => 1,
+	'api_enable_basic_auth' => true,
+	'api_oauth2_access_token_lifetime' => 60,
+	'api_oauth2_refresh_token_lifetime' => 14,
+	'api_batch_max_limit' => '200',
+	'upload_dir' => '/home/nbush/workspace/mautic-eb/mautic/app/../media/files',
+	'max_size' => '6',
+	'allowed_extensions' => array(
+		"0" => "csv", 
+		"1" => "doc", 
+		"2" => "docx", 
+		"3" => "epub", 
+		"4" => "gif", 
+		"5" => "jpg", 
+		"6" => "jpeg", 
+		"7" => "mpg", 
+		"8" => "mpeg", 
+		"9" => "mp3", 
+		"10" => "odt", 
+		"11" => "odp", 
+		"12" => "ods", 
+		"13" => "pdf", 
+		"14" => "png", 
+		"15" => "ppt", 
+		"16" => "pptx", 
+		"17" => "tif", 
+		"18" => "tiff", 
+		"19" => "txt", 
+		"20" => "xls", 
+		"21" => "xlsx", 
+		"22" => "wav"
+	),
+	'campaign_time_wait_on_event_false' => 'PT1H',
+	'mailer_return_path' => null,
+	'mailer_append_tracking_pixel' => 1,
+	'mailer_convert_embed_images' => 0,
+	'mailer_amazon_region' => 'email-smtp.us-east-1.amazonaws.com',
+	'mailer_spool_msg_limit' => null,
+	'mailer_spool_time_limit' => null,
+	'mailer_spool_recover_timeout' => '900',
+	'mailer_spool_clear_timeout' => '1800',
+	'unsubscribe_text' => '<a href=\'|URL|\'>Unsubscribe</a> to no longer receive emails from us.',
+	'webview_text' => '<a href=\'|URL|\'>Having trouble reading this email? Click here.</a>',
+	'unsubscribe_message' => 'We are sorry to see you go! |EMAIL| will no longer receive emails from us. If this was by mistake, <a href=\'|URL|\'>click here to re-subscribe</a>.',
+	'resubscribe_message' => '|EMAIL| has been re-subscribed. If this was by mistake, <a href=\'|URL|\'>click here to unsubscribe</a>.',
+	'monitored_email' => array(
+		"general" => array(
+			"address" => "", 
+			"host" => "", 
+			"port" => "993", 
+			"encryption" => "/ssl", 
+			"user" => "", 
+			"password" => ""
+		), 
+		"EmailBundle_bounces" => array(
+			"address" => "", 
+			"host" => "", 
+			"port" => "993", 
+			"encryption" => "/ssl", 
+			"user" => "", 
+			"password" => "", 
+			"override_settings" => "0", 
+			"folder" => ""
+		), 
+		"EmailBundle_unsubscribes" => array(
+			"address" => "", 
+			"host" => "", 
+			"port" => "993", 
+			"encryption" => "/ssl", 
+			"user" => "", 
+			"password" => "", 
+			"override_settings" => "0", 
+			"folder" => ""
+		), 
+		"EmailBundle_replies" => array(
+			"address" => "", 
+			"host" => "", 
+			"port" => "993", 
+			"encryption" => "/ssl", 
+			"user" => "", 
+			"password" => "", 
+			"override_settings" => "0", 
+			"folder" => ""
+		)
+	),
+	'mailer_is_owner' => 0,
+	'default_signature_text' => 'Best regards, |FROM_NAME|',
+	'email_frequency_number' => null,
+	'email_frequency_time' => null,
+	'show_contact_preferences' => 0,
+	'show_contact_frequency' => 0,
+	'show_contact_pause_dates' => 0,
+	'show_contact_preferred_channels' => 0,
+	'show_contact_categories' => 0,
+	'show_contact_segments' => 0,
+	'mailer_mailjet_sandbox' => 0,
+	'mailer_mailjet_sandbox_default_mail' => null,
+	'disable_trackable_urls' => 0,
+	'parallel_import_limit' => '1',
+	'background_import_if_more_rows_than' => 0,
+	'cat_in_page_url' => 0,
+	'google_analytics' => null,
+	'track_contact_by_ip' => 0,
+	'track_by_tracking_url' => 1,
+	'track_by_fingerprint' => 0,
+	'facebook_pixel_id' => null,
+	'facebook_pixel_trackingpage_enabled' => 0,
+	'facebook_pixel_landingpage_enabled' => 0,
+	'google_analytics_id' => null,
+	'google_analytics_trackingpage_enabled' => 0,
+	'google_analytics_landingpage_enabled' => 0,
+	'rabbitmq_host' => 'localhost',
+	'rabbitmq_port' => '5672',
+	'rabbitmq_vhost' => '/',
+	'rabbitmq_user' => 'guest',
+	'rabbitmq_password' => 'guest',
+	'beanstalkd_host' => 'localhost',
+	'beanstalkd_port' => '11300',
+	'beanstalkd_timeout' => '60',
+	'saml_idp_entity_id' => 'http://mautic.local',
+	'saml_idp_own_password' => null,
+	'saml_idp_email_attribute' => 'EmailAddress',
+	'saml_idp_username_attribute' => null,
+	'saml_idp_firstname_attribute' => 'FirstName',
+	'saml_idp_lastname_attribute' => 'LastName',
+	'saml_idp_default_role' => null,
+	'webhook_start' => '0',
+	'webhook_limit' => '10',
+	'webhook_log_max' => '1000',
+	'webhook_disable_limit' => '100',
+	'webhook_timeout' => '15',
+	'queue_mode' => 'immediate_process',
+	'events_orderby_dir' => 'ASC',
+	'twitter_handle_field' => 'twitter',
+	'disable_lead_table_fields' => 1,
+	'transifex_username' => null,
+	'store_as' => 'abbreviation',
+	'display_as' => 'properName',
+);
