b4d32858db29cfcc333ced65c3975f57b20d8390
heathdutton <hdutton@thedmsgrp.com>
2018-06-11 01:03:25 -0400

    Shift to using workers for campaign event triggers.
    By default run 10 concurrent modulus workers.

---
+++ scripts/mautic-worker.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Runs multiple concurrent threads of campaign event triggers.
+
+# Check dependencies.
+if [ -z $( which ps ) ]
+then
+    echo "ps is required to run this script."
+    exit 1
+fi
+if [ -z $( which grep ) ]
+then
+    echo "grep is required to run this script."
+    exit 1
+fi
+if [ -z $( which nohup ) ]
+then
+    echo "nohup is required to run this script."
+    exit 1
+fi
+
+# Load environment variables (which may change during execution).
+if [ -f "/opt/elasticbeanstalk/support/envvars" ]
+then
+    . /opt/elasticbeanstalk/support/envvars
+fi
+
+# Establish the current worker count based on environment variables.
+if [ -z "$MAUTIC_WORKERS" ]
+then
+    workers=10
+else
+    workers=$MAUTIC_WORKERS
+fi
+
+# Get the count of instances running this script (including the current one).
+for (( i = 1; i <= $workers; i++ ))
+do
+    cronloop mautic:campaign:trigger --thread-id $i --max-threads $workers --no-ansi --no-interaction --quiet &
+done
