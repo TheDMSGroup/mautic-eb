de78a00036f28569555d6276c1dde5f59af4d107
heathdutton <hdutton@thedmsgrp.com>
2018-12-27 16:29:02 -0500

    Flush and warm cache after build.

--- scripts/build-local.sh
+++ scripts/build-local.sh
@@ -193,4 +193,8 @@ then
     cp /tmp/mautic_local.php ./mautic/app/config/local.php
     rm -rf ./mautic/app/config/parameters_local.php
     rm -rf /tmp/mautic_local.php
+    echo ; echo "Flushing cache"
+    rm -rf ./mautic/app/cache/*
+    echo ; echo "Warming cache"
+    php ./mautic/app/console cache:warm --no-optional-warmers --quiet
 fi
